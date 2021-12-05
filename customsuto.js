9921456214fb84ac8d76aa2b39dd12b469a8f8ce
Spyboy <itisshubhamkumar@gmail.com>
2021-12-05 12:11:42 +0530

    Create customsuto.js

---
+++ customsuto.js
@@ -0,0 +1,35 @@
+// FUNCTION CODE
+function gjCountAndRedirect(secounds, url) {
+
+  $('#gj-counter-num').text(secounds);
+
+  $('#gj-counter-box').show();
+
+  var interval = setInterval(function() {
+
+    secounds = secounds - 1;
+
+    $('#gj-counter-num').text(secounds);
+
+    if (secounds == 0) {
+
+      clearInterval(interval);
+      window.location = "https://spyboy.blog/";
+      $('#gj-counter-box').hide();
+
+    }
+
+  }, 1000);
+
+  $('#gj-counter-box').click(function() {
+    clearInterval(interval);
+    window.location = "https://spyboy.blog/";
+
+  });
+}
+
+// USE EXAMPLE
+$(document).ready(function() {
+  //call
+  gjCountAndRedirect(10, document.URL);
+});
