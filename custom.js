91dc4f0ca2024f985c1c0617fb239f96072618eb
Spyboy <itisshubhamkumar@gmail.com>
2021-12-05 11:33:50 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,35 @@
-<script>
- setTimeout(function(){
-    window.location.href = 'https://spyboy.blog/';
- }, 5000);
-</script>
+// FUNCTION CODE
+function gjCountAndRedirect(secounds, url) {
+
+  $('#gj-counter-num').text(secounds);
+
+  $('#gj-counter-box').show();
+
+  var interval = setInterval(function() {
+
+    secounds = secounds - 1;
+
+    $('#gj-counter-num').text(secounds);
+
+    if (secounds == 0) {
+
+      clearInterval(interval);
+      window.location = url;
+      $('#gj-counter-box').hide();
+
+    }
+
+  }, 1000);
+
+  $('#gj-counter-box').click(function() {
+    clearInterval(interval);
+    window.location = url;
+
+  });
+}
+
+// USE EXAMPLE
+$(document).ready(function() {
+  //call
+  gjCountAndRedirect(10, document.URL);
+});
