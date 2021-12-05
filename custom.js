10e61cbdbb1e0c0169d19e72b991a5ad9d0812f2
Spyboy <itisshubhamkumar@gmail.com>
2021-12-05 11:44:58 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,35 +1,49 @@
 // FUNCTION CODE
-function gjCountAndRedirect(secounds, url) {
+function gjCountAndRedirect(secounds, url)
+{
 
-  $('#gj-counter-num').text(secounds);
+		$('#gj-counter-num').text(secounds);
 
-  $('#gj-counter-box').show();
+		$('#gj-counter-box').show();
 
-  var interval = setInterval(function() {
+	var interval = setInterval(function()
+	{
 
-    secounds = secounds - 1;
+		secounds = secounds - 1;
 
-    $('#gj-counter-num').text(secounds);
+		$('#gj-counter-num').text(secounds);
 
-    if (secounds == 0) {
+		if(secounds == 0)
+		{
 
-      clearInterval(interval);
-      window.location = url;
-      $('#gj-counter-box').hide();
+			clearInterval(interval);
+			window.location = url;
+			$('#gj-counter-box').hide();
+		
+		}
+		
+	}, 1000);
 
-    }
-
-  }, 1000);
-
-  $('#gj-counter-box').click(function() {
-    clearInterval(interval);
-    window.location = url;
-
-  });
+	$('#gj-counter-box').click(function()
+	{
+		clearInterval(interval);
+		window.location = url;
+	
+	});
 }
 
 // USE EXAMPLE
 $(document).ready(function() {
-  //call
-  gjCountAndRedirect(10, document.URL);
+	//var
+	var gjCountAndRedirectStatus = false; //prevent from seting multiple Interval
+	
+	//call
+	$('h1').click(function(){
+		if(gjCountAndRedirectStatus == false)
+		{
+			gjCountAndRedirect(10, document.URL);
+			gjCountAndRedirectStatus = true;
+		}
+	});
+	
 });
